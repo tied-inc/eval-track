b377f3fb08b70e75d596a91ff20fe34d15ef2249
hayata-yamamoto <hayata.yamamoto.work@gmail.com>
2025-01-01 22:09:27 +0900

    feat: add web app

--- tracker-py/tracker/storage/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Storage package for eval-track."""
-
-from .s3 import S3Storage
-from .storage import AbstractObjectStorage, StorageError
-
-__all__ = ["AbstractObjectStorage", "StorageError", "S3Storage"]
